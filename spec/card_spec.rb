ecad5a14bfa439a5bb908aee0c29138589c800df
Beta-23 <alfred.dominguez@gmail.com>
2019-09-28 20:48:03 -0700

    Reduce rspec duplication code using the let method to minimize total memory used during test

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,6 +1,6 @@
 class Card
 
-    attr_reader :rank, :suit
+    attr_accessor :rank, :suit
 
     def initialize(rank, suit)  
         @rank = rank
@@ -10,19 +10,30 @@ end
 
 # RSpec module with the describe method
 RSpec.describe Card do 
-    # before action, runs individual rspc example before moving on to the next one
-    before do
-        @card = Card.new('Seven', 'Clubs')  
-    end
+    # before action block (hooks) with instance variable, runs individual rspc example before moving on to the next one
+    # before do
+    #     @card = Card.new('Seven', 'Clubs')  
+    # end
+
+    # def card
+    #     Card.new('Seven', 'Clubs')
+    # end
+
+    # Method invokes the symbol :card and instantiating the new block cashed to the var card
+    # Minimizing the run time of examples tested and reduce total memory used/cahsed
+    let(:card) { Card.new('Seven', 'Clubs') }
+
     # the it (specify) method describes how the value behaves or how it works
-    it 'has a rank' do 
+    it 'has a rank and rank can change' do 
         # The expect method takes in an argument or assertion
-        expect(@card.rank).to eq('Seven')
+        expect(card.rank).to eq('Seven')
+        card.rank = 'Queen'
+        expect(card.rank).to eq('Queen')
     end
 
     it 'has a suit' do
         # The expect method takes in an argument or assertion
-        expect(@card.suit).to eq('Clubs') 
+        expect(card.suit).to eq('Clubs') 
     end
 end
 
